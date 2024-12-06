0781134cd5090a501729828ac690a1f49ed91eda
priyanka680shaw <priyankashaw680@gmail.com>
2024-12-06 11:52:40 +0530

    adding header folder

--- src/app/layouts/header/headerOne/headerOne.js
+++ src/app/layouts/header/headerOne/headerOne.js
@@ -2,20 +2,25 @@ import headeroneStyle from "./headerOneStle.module.css"
 import Wrapper from "../../wrapper/wrapper"
 import headerOneStyle from "./headerOneStle.module.css"
 export default function HeaderOne(){
+
+    const headerData = ["data1" , "data2" , "data3" , "data4" , "data4" , "data5"]
+
   return(
     <>
+    <marquee >
     <div className = {headerOneStyle.mainContainner}>
-      {/* <Wrapper style={{border : "2px solid green" , margin : "auto" }}> */}
-      <Wrapper>
-        <div>
-          <h1>Header one </h1>
-        </div>
-      </Wrapper>
-    </div>
-    <hr style={{ border: "1px solid rgb(218, 214, 214)", margin: "2px 0px" , width : "100%" }} />    
-     
-   
-      {/* <h1 style={{headeroneStyle.color}}>Hello Header one</h1> */}
+        {
+          headerData.map((items , index)=>{
+             return(
+              <p key={index} style={{color :"red"}}>{items}</p>
+             )
+          })
+        }
+      </div>
+
+    </marquee>
+    
+      <hr style={{ border: "1px solid rgb(218, 214, 214)", margin: "2px 0px" , width : "100%" }} />    
     </>
   )
 }
