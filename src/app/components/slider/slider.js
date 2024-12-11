a39e16591fab2e2e758c7cf9674789cfdfe6a4f7
priyanka680shaw <priyankashaw680@gmail.com>
2024-12-11 23:58:38 +0530

    adding carousel

--- src/app/components/slider/slider.js
+++ src/app/components/slider/slider.js
@@ -1,20 +1,56 @@
-import SliderCard from "./sliderCard"
-import sliderStyle from "./sliderCard.module.css"
-import Wrapper from "@/app/layouts/wrapper/wrapper"
-import sliderBg from "../../images/slider/sliderBg.png"
-export default function SliderPage(){
-    return(
-        <>
-        <section className= {sliderStyle.sliderContainer}>
-            <div className= {sliderStyle.mainContainer} style={{margin  :"auto"}}>
-              <Wrapper style={{display  :"flex" , justifyContent  :"center" , alingItem : "center"}}>
-                <SliderCard/>
-                <SliderCard/>
-                <SliderCard/>
-              </Wrapper>
+'use client';
+import SliderCard from "./sliderCard";
+import sliderStyle from "./sliderCard.module.css";
+import Wrapper from "@/app/layouts/wrapper/wrapper";
+import sliderImage from "../../images/slider/slider.webp";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function SliderPage() {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        arrows : false,
+        responsive: [
+            { breakpoint: 1440, settings: { slidesToShow: 5 } },
+            { breakpoint: 1024, settings: { slidesToShow: 3 } },
+            { breakpoint: 768, settings: { slidesToShow: 2 } },
+            { breakpoint: 600, settings: { slidesToShow: 1 } },
+        ],
+    };
+
+    const sliderDemo = [
+        { image: sliderImage, heading: "Markets", description: "Budget 2024: Sitharaman Prioritizes" },
+        { image: sliderImage, heading: "News", description: "Budget 2024: Sitharaman Prioritizes Infrastructure" },
+        { image: sliderImage, heading: "Finance", description: "Budget 2024:  Infrastructure" },
+        { image: sliderImage, heading: "News", description: "Budget 2024: Sitharaman Prioritizes Infrastructure" },
+        { image: sliderImage, heading: "Finance", description: "Budget 2024: Sitharaman Prioritizes Infrastructure" },
+    ];
+
+    return (
+        <section className={sliderStyle.sliderContainer}>
+            <div className={sliderStyle.mainContainer}>
+                <Wrapper>
+                    <Slider {...settings}>
+                        {sliderDemo.map((item, index) => (
+                            <SliderCard
+                                key={index}
+                                image={item.image}
+                                heading={item.heading}
+                                description={item.description}
+                            />
+                        ))}
+                    </Slider>
+                </Wrapper>
             </div>
         </section>
-            
-        </>
-    )
+    );
 }
+ 
--- src/app/components/slider/sliderCard.js
+++ src/app/components/slider/sliderCard.js
@@ -2,20 +2,24 @@ import Wrapper from "@/app/layouts/wrapper/wrapper"
 import slideCardStyle from "./sliderCard.module.css"
 import Image from "next/image"
 import sliderimg from "../../images/slider/slider.webp"
-export default function SliderCard(){
+export default function SliderCard({image , heading ,description}){
+    // console.log(image.src , heading , description)
     return(
+        //1.if image is given addd fall back image
         <>
             <section>
                 <div className={slideCardStyle.cardContainer}>
                     <figure className={slideCardStyle.innerContainer}>
                         <div className= {slideCardStyle.imageContainer}>
-                             <Image src={sliderimg} alt="Image description" className={slideCardStyle.image} />
+                             <Image src={image} alt="Image description" className={slideCardStyle.image} />
                         </div>
-                        <figcaption>
-                            <h3 className= {slideCardStyle.imgCaptionh3}>Image Header</h3>
-                            <p className={slideCardStyle.imgCaptionP}>Date: 2024-12-10</p>
-                            <p className={slideCardStyle.imgCaptionP}>This is a brief description about the image.</p>
-                        </figcaption>
+                        <div className={slideCardStyle.captioncContainer}>
+                            <figcaption>
+                                <h3 className= {slideCardStyle.imgCaptionh3}>{heading}</h3>
+                                {/* <p className={slideCardStyle.imgCaptionP}>{}</p> */}
+                                <p className={slideCardStyle.imgCaptionP}>{description}</p>
+                            </figcaption>
+                        </div>
                     </figure>
                 </div>
             </section>
