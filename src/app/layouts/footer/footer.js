cafa6bbabb696f16e8e0838b387d1b8d2b0af35a
priyanka680shaw <priyankashaw680@gmail.com>
2025-01-06 16:29:07 +0530

    footer changes

--- src/app/layouts/footer/footer.js
+++ src/app/layouts/footer/footer.js
@@ -76,43 +76,8 @@ const footerData = [
 ];
 
 const Footer = () => {
-  // return (
-  //   <div className="bg-blue-900 w-[100%] border-red-900">
-  //     <Wrapper style= "margin : auto">
-  //       <footer className=" text-white w-[100%]">
-  //         <div className="container mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 px-4">
-  //           {footerData.map((section, index) => (
-  //             <div key={index}>
-  //               <h3 className="text-lg font-bold mb-4">{section.title}</h3>
-  //               <ul className="space-y-2">
-  //                 {section.links.map((link, idx) => (
-  //                   <li key={idx} className="text-sm hover:underline">
-  //                     {link}
-  //                   </li>
-  //                 ))}
-  //               </ul>
-  //             </div>
-  //           ))}
-  //         </div>
-  //         <div className="border-t border-gray-700 mt-8 pt-4 px-4 text-center">
-  //           <p>
-  //             Stocks: A | B | C | D | E | F | G | H | I | J | K | L | M | N | O
-  //             | P | Q | R | S | T | U | V | W | X | Y | Z | Others
-  //           </p>
-  //           <p>
-  //             Mutual Funds: A | B | C | D | E | F | G | H | I | J | K | L | M |
-  //             N | O | P | Q | R | S | T | U | V | W | X | Y | Z | Others
-  //           </p>
-  //           <div className="flex justify-center mt-4 space-x-4">
-  //             {/* <Image src="/appstore.png" alt="App Store" className="h-10" />
-  //         <Image src="/googleplay.png" alt="Google Play" className="h-10" />
-  //         <Image src="/windowsstore.png" alt="Windows Store" className="h-10" /> */}
-  //           </div>
-  //         </div>
-  //       </footer>
-  //     </Wrapper>
-  //   </div>
-  // );
+             <h3 className="text-lg font-bold mb-4">{section.title}</h3>
+ 
 
   const iconSize = 30; // Adjust size as needed
 
